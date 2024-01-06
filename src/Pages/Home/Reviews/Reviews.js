4063b1b53cfd4ef15044d1b8e468402662116987
Authoi234 <jawadkhan.authoi@gmail.com>
2024-01-07 00:23:22 +0600

    half review section is complete

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import quote from '../../../assets/icons/quote.svg';
+import people1 from '../../../assets/images/people1.png';
+import people2 from '../../../assets/images/people2.png';
+import people3 from '../../../assets/images/people3.png';
+import Review from './Review';
 
 const Reviews = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'California',
+            img: people1
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'California',
+            img: people2
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            review: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location: 'California',
+            img: people3
+        },
+    ]
     return (
-        <div>
+        <div className='mt-20'>
             <div className='flex justify-between'>
                 <div>
-                    <p></p>
-                    <h1></h1>   
+                    <p className='text-secondary font-bold text-2xl'>Testimonial</p>
+                    <h1 className='text-4xl'>What Our Patients Says</h1>   
                 </div>
                 <div>
-                    <img src={quote} alt="" />
+                    <img className='w-2/3' src={quote} alt="" />
                 </div>
             </div>
-            <div></div>
+            <div className='mt-20'>
+                {
+                    reviews.map(review => <Review key={review._id} review={review}></Review>)
+                }
+            </div>
         </div>
     );
 };
