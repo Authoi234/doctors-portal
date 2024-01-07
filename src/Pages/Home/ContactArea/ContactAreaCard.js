d68d7310b8c5183e868b3f6d4d25b933d9985f3d
Authoi234 <jawadkhan.authoi@gmail.com>
2024-01-07 21:47:51 +0600

    appointment is on developing

--- src/Pages/Home/ContactArea/ContactAreaCard.js
+++ src/Pages/Home/ContactArea/ContactAreaCard.js
@@ -4,7 +4,7 @@ import clockImg from "../../../assets/icons/clock.svg"
 const ContactAreaCard = ({ card }) => {
     const { icon, discription, name, bgClass } = card
     return (
-        <div className={`sm:block md:flex justify-center items-center break-words p-5 rounded-lg text-white sm:w-full md:w-1/3 lg:w-1/4 md:mx-5 sm:my-5 my-5 ${bgClass}`}>
+        <div className={`sm:block md:flex justify-center items-center break-words md:p-5 p-1 rounded-lg text-white sm:w-full md:w-1/3 lg:w-1/4 md:mx-5 mx-1 sm:my-5 my-5 ${bgClass}`}>
             <img className='pr-3' src={icon} alt="" />
             <div>
                 <h1>{name}</h1>
