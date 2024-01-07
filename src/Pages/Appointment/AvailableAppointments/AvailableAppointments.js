f2b894aae3b4d6140f92981a6f105cf794a6547d
Authoi234 <jawadkhan.authoi@gmail.com>
2024-01-07 22:56:26 +0600

    loading data is complete

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,24 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({selectedDate}) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+        .then(res => res.json())
+        .then(data => setAppointmentOptions(data))
+    }, [])
+
     return (
         <section className='my-16'>
             <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div>
+                {
+                    appointmentOptions.map(option => <AppointmentOption key={option._id}></AppointmentOption>)
+                }
+            </div>
         </section>
     );
 };
