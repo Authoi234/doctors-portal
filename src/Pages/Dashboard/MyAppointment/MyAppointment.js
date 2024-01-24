04c5b69f9a72235ae133bafdece38a9502392554
Authoi234 <jawadkhan.authoi@gmail.com>
2024-01-24 17:42:46 +0600

    table is added

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const MyAppointment = () => {
+    const {user} = useContext(AuthContext);
+    const url = `http://localhost:5000/bookings?email=${user?.email}`
+
     return (
         <div>
-            <h3 className="text-3xl">My Appointments</h3>
+            <h3 className="text-3xl mb-5">My Appointments</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead className='bg-base-300'>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='hover'>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Blue</td>
+                        </tr>
+                        <tr className="hover">
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Purple</td>
+                        </tr>
+                        <tr className='hover'>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Red</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
